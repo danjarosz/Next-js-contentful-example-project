3715784f9791a7180081821eb954f0581091f606
danjarosz <jarosz.daniel@protonmail.com>
2021-06-13 20:23:09 +0200

    feat: setup Contentful client

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
-export default function Recipes() {
+import { createClient } from "contentful";
+
+export const getStaticProps = async () => {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  });
+
+  const response = await client.getEntries({
+    content_type: "recipe",
+  });
+
+  return {
+    props: {
+      recipes: response.items,
+    },
+  };
+};
+
+export default function Recipes({ recipes }) {
+  console.log(recipes);
   return <div className="recipe-list">Recipe List</div>;
 }
