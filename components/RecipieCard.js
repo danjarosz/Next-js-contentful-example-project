f988cead13aa253467333e7de52023b07cbed91f
danjarosz <jarosz.daniel@protonmail.com>
2021-06-14 21:50:40 +0200

    feat: render images from external source

--- components/RecipieCard.js
+++ components/RecipieCard.js
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const RecipieCard = ({ recipe }) => {
   const { title, slug, cookingTIme, thumbnail } = recipe.fields;
 
   return (
     <div className="card">
-      <div className="featured"></div>
+      <div className="featured">
+        <Image
+          src={`https:${thumbnail.fields.file.url}`}
+          width={thumbnail.fields.file.details.image.width}
+          height={thumbnail.fields.file.details.image.height}
+        />
+      </div>
       <div className="content">
         <div className="info">
           <h4>{title}</h4>
