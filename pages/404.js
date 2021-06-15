55f40fc48dc2769b6f8127d1a1456492b906cd90
danjarosz <jarosz.daniel@protonmail.com>
2021-06-15 21:16:04 +0200

    feat: custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timeoutID = setTimeout(() => {
+      router.push("/");
+    }, 4000);
+
+    return () => {
+      clearTimeout(timeoutID);
+    };
+  }, []);
+
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <h2>Ooooops! That bage cannot be found :(</h2>
+      <p>
+        Redirecting to{" "}
+        <Link href="/">
+          <a>HomePage</a>
+        </Link>{" "}
+        for more marmite goodness...
+      </p>
+
+      <style jsx>{`
+        .not-found {
+          background: #fff;
+          padding: 30px;
+          box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+          transform: rotateZ(-1deg);
+        }
+        h1 {
+          font-size: 3em;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default NotFound;
